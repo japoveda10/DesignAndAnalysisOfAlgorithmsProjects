e7a5af8ee0c6dea4f7e2aa3488958fe6cbc64978
japoveda10 <ja.poveda10@uniandes.edu.co>
2017-06-27 10:29:49 -0500

    matrix package added

---
+++ Activity/matrix/MatrixIterator1.java
@@ -0,0 +1,50 @@
+package matrix;
+
+import java.util.Iterator;
+
+/**
+ * Class that represents an objects matrix iterator
+ * Goes through each element of the matrix by rows
+ * @author David Cortés and Julio Poveda
+ */
+public class MatrixIterator1 implements Iterator
+{
+	Object[][] matrix;
+	int i;
+	int j;
+	
+	public MatrixIterator1(Object[][] pObjectsMatrix)
+	{
+		matrix = pObjectsMatrix;
+		i = 0;
+		j = 0;
+	}
+	
+	@Override
+	public boolean hasNext() 
+	{
+		return j>=0;
+	}
+
+	@Override
+	public Object next() 
+	{
+		Object object = null;
+		
+		if(j < matrix[0].length)
+		{
+			System.out.println("index i = " + i);
+			System.out.println("index j = " + j);
+			object = matrix[i][j];
+			j += 1;			
+		}
+		else if(j == matrix.length)
+		{
+			j = 0;
+			i += 1;
+			object = matrix[i][j];			
+		}
+		
+		return object;
+	}
+}
---
+++ Activity/matrix/MatrixIterator2.java
@@ -0,0 +1,50 @@
+package matrix;
+
+import java.util.Iterator;
+
+/**
+ * Class that represents an objects matrix iterator
+ * Goes through each element of the matrix by columns
+ * @author David Cortés and Julio Poveda
+ */
+public class MatrixIterator2 implements Iterator
+{
+	Object[][] matrix;
+	int i;
+	int j;
+	
+	public MatrixIterator2(Object[][] pObjectsMatrix)
+	{
+		matrix = pObjectsMatrix;
+		i = 0;
+		j = 0;
+	}
+	
+	@Override
+	public boolean hasNext() 
+	{
+		return i>=0;
+	}
+
+	@Override
+	public Object next() 
+	{
+		Object object = null;
+		
+		if(i < matrix.length)
+		{
+			System.out.println("index i = " + i);
+			System.out.println("index j = " + j);
+			object = matrix[i][j];
+			i += 1;			
+		}
+		else if(i == matrix.length)
+		{
+			i = 0;
+			j += 1;
+			object = matrix[i][j];			
+		}
+		
+		return object;
+	}
+}
